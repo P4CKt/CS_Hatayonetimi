3938d096f2abe1f0bb81432770dd623394f3d7d6
P4CKt <omurcanuslu@outlook.com>
2022-12-07 00:53:54 +0300

    değişti

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace odev_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Try catch blokları sayesinde uygulama içerisinde bir hata oluştuğunda belirtilen işlemler yaptırılabilir.
+
+            try{ Hataya sebebiyet verme ihtimali olan kod }
+
+            catch { Hata ile karşılaşıldığında ne yapılacağı buraya yazılır }
+
+            finally{ Hata olsun olmasın mutlaka yapılmasını istediğimiz işler varsa buraya yazarız }*/
+            
+            Console.WriteLine("Bir Sayı Giriniz!");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girdiğiniz Sayı: "+ sayi);
+            Console.Read();
+
+
+        }
+    }
+}
